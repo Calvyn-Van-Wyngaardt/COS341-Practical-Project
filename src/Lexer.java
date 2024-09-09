a3bcb14e15220192b08bfe1eee71774f00d69eee
Sud0man1 <calvyntbx@gmail.com>
2024-09-09 11:16:19 +0200

    Fixed erroneous lexing. Added files

--- src/Lexer.java
+++ src/Lexer.java
@@ -4,20 +4,32 @@ public class Lexer {
     ArrayList<String> tokens;
     int tokenCount;
 
-    public Lexer(String input) {
+    public Lexer(String[] input) {
         System.out.println("Input:\n" + input);
         this.tokens = tokenizeInput(input);
         this.tokenCount = this.tokens.size();
     }
 
-    private ArrayList<String> tokenizeInput(String input) {
+    private ArrayList<String> tokenizeInput(String[] input) {
         ArrayList<String> tokens = new ArrayList<>();
 
-        //Reformat string so that all excessive whitespace & newlines are reduced to a single " "
-        input = input.replaceAll("\\s+", " ");
-        input = input.replaceAll("\\n+", " ");
-        String tokenStrings[] = input.split(" ");
-        tokens.addAll(Arrays.asList(tokenStrings));
+        // Reformat string so that all excessive whitespace & newlines are reduced to a single " "
+        // Additional checks are added to cater for all platforms
+        for (int i = 0; i < input.length; i++) {
+            input[i] = input[i].replaceAll("[\\s]+", " ");
+            input[i] = input[i].replaceAll("[\\r\\n]+", " ");
+            input[i] = input[i].replaceAll(",", " , ");
+            input[i] = input[i].replaceAll("[\\(]+", " ( ");
+            input[i] = input[i].replaceAll("[\\)]+", " ) ");
+            
+            String tokenStrings[] = input[i].split(" ");
+            
+            for (String s : tokenStrings) {
+                if (!s.matches("[\\s]*")) {
+                    tokens.add(s);
+                }
+            }
+        }
 
         return tokens;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,41 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String input = "main\n\t\tbegin\t skip\t\nend\t\n";
-        Lexer lexer = new Lexer(input);
-        System.out.println(lexer);
+        File inputDir = new File("../input");
+        File inputFiles[] = inputDir.listFiles();
+        ArrayList<String[]> inputs = new ArrayList<>();
 
+        for (int i = 0; i < inputFiles.length; i++) {
+            inputs.add(readFile(inputFiles[i]));
+        }
+
+        Lexer lexer = new Lexer(inputs.get(1));
+        System.out.println(lexer);
+    }
+
+    private static String[] readFile(File inputFile) {
+        try {
+            ArrayList<String> lines = new ArrayList<>();
+            Scanner scan = new Scanner(inputFile);
+            while (scan.hasNextLine()) {
+                lines.add(scan.nextLine());
+            }        
+
+            String output[] = new String[lines.size()];
+            for (int i = 0; i < lines.size(); i++) {
+                output[i] = lines.get(i);
+            }
+
+            scan.close();
+            return output;
+
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+
+        return null;
     }
 }
