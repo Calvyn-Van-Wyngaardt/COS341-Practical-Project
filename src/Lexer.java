c3d51be8e46e4a5884d6cd86c21c66888ce756f9
Sud0man1 <calvyntbx@gmail.com>
2024-10-16 15:39:05 +0200

    Updated lexer

--- src/Lexer.java
+++ src/Lexer.java
@@ -3,6 +3,10 @@ import java.util.*;
 public class Lexer {
     private final ArrayList<Token> tokens;
     private int tokenCount;
+    private static final String TOKEN_V = "V_[a-z][a-z0-9]";
+    private static final String TOKEN_F = "F_[a-z][a-z0-9]";
+    private static final String TOKEN_T = "[A-Z][a-z]{0,7}";
+    private static final String TOKEN_N = "-?[0-9]+(\\.[0-9]+)?";
 
     public Lexer(String[] input) {
         this.tokenCount = 0;
@@ -33,21 +37,43 @@ public class Lexer {
                         //  Checking for terminals  //
                         //////////////////////////////
 
-                        if (j < tokenStrings.length) {
-                            if (tokenStrings[j].contains("<")) {
-
-                                //Does not contain space (E.G "V_abc <helo")
-                                if (tokenStrings[j].length() < 2) {
-                                    tokenValue = String.format("< %s", tokenStrings[j+1]);
-                                    j++;                            //Skip redundant input
+                        // User input
+                        if (tokenStrings[j].contains("<")) {
+                            if (j+1 < tokenStrings.length) {
+                                tokenValue = "< ";
+                                
+                                try {
+                                    while (!tokenStrings[j].equals(";")) {
+                                        tokenValue += tokenStrings[j] + " ";
+                                        j++;
+                                    }
+                                    tokenStrings[j] = tokenStrings[j].strip();
+                                } catch (Exception e) {
+                                    System.out.println("ERROR: No \";\" found for user input");
                                 }
 
+
+                                tokenValue = String.format("< %s", tokenStrings[j+1]);
                                 tokenClass = "user_input";
                             }
-                        } 
-                        // Edge case: V_abc <
-                        else if (j < tokenStrings.length && tokenValue.contains("<")) {
-                            throw new Error(String.format("ERROR: Character '<' should be followed by a value for the variable name"));
+                            else {
+                                throw new Error(String.format("ERROR: Character '<' should be followed user input for the variable name"));
+                            }
+                        } else if (tokenStrings[j].matches(TOKEN_V)) {
+                            tokenValue = tokenStrings[j];
+                            tokenClass = "Variable_Name";
+                        } else if (tokenStrings[j].matches(TOKEN_F)) {
+                            tokenValue = tokenStrings[j];
+                            tokenClass = "Function_Name";
+                        } else if (tokenStrings[j].matches(TOKEN_T)) {
+                            tokenValue = tokenStrings[j];
+                            tokenClass = "String_Token";
+                        } else if (tokenStrings[j].matches(TOKEN_N)) {
+                            tokenValue = tokenStrings[j];
+                            tokenClass = "Number_Token";
+                        }
+                        else {
+                            throw new Error(String.format("ERROR: \"%s\" not found in grammar", tokenStrings[j]));
                         }
                         
                         inputTokens.add(new Token(tokenClass, tokenValue, this.tokenCount++));
